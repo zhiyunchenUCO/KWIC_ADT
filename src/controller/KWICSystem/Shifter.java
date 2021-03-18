6cd9c06d048a83ba8c079b3b60f25b718aa03bc1
Zhiyun Chen <zchen6@uco.edu>
2021-03-18 10:44:24 -0500

    UML diagrams deleted

--- src/controller/KWICSystem/Shifter.java
+++ src/controller/KWICSystem/Shifter.java
@@ -2,54 +2,8 @@ package controller.KWICSystem;
 
 import java.util.ArrayList;
 
-public class Shifter {
-
-    Lines lines;
-    ArrayList<int[]> wordIndices;
-
-    public void setup(Lines lines) {
-
-        this.lines = lines;
-        wordIndices = new ArrayList<>();
-        setWordIndices();
-    }
-
-    private void setWordIndices() {
-
-        for (int i = 0; i < lines.getLineCount(); i++) {
-            String line = lines.getLine(i);
-
-            // Add the position of the first word to word indices
-            int[] startWordIndex = new int[]{i, 0};
-            wordIndices.add(startWordIndex);
-
-            // Add positions of the following words to word indices
-            for (int j=0; j<line.length(); j++) {
-                if (line.charAt(j) == ' ') {
-                    wordIndices.add(new int[]{i, j+1});
-                }
-            }
-        }
-    }
-
-    public ArrayList<int[]> getWordIndices() {
-        return  wordIndices;
-    }
-
-    public String getCirculatedLine(int[] wordIndex) {
-        // Circular read a line, starting at a given char index
-        int lineIndex = wordIndex[0];
-        int charIndex = wordIndex[1];
-        String line = lines.getLine(lineIndex);
-
-        if (charIndex == 0) {
-            return  line;
-        }
-        int lineLength = line.length();
-        return line.substring(charIndex, lineLength) + " " + line.substring(0,charIndex);
-    }
-
-    public Lines getLines() {
-        return lines;
-    }
+public interface Shifter {
+    void setup(Lines lines);
+    ArrayList<int[]> getWordIndices();
+    String getCirculatedLine(int[] wordIndex);
 }
--- src/controller/KWICSystem/Sorter.java
+++ src/controller/KWICSystem/Sorter.java
@@ -1,93 +1,10 @@
 package controller.KWICSystem;
 
-import java.io.*;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 
-/**
- * This is another implementation of Filter.  It implements the
- * sorting of lines according to a custom-defined sorting algorithm.
- **/
-public class Sorter {
+public interface Sorter {
 
-    Shifter circularShifter;
-    ArrayList<int[]> sortedWordIndices;
-
-    public void sort(Shifter circularShifter) throws IOException {
-        this.circularShifter = circularShifter;
-        setSortedWordIndices();
-    }
-
-    public ArrayList<int[]> getSortedWordIndices() {
-        return sortedWordIndices;
-    }
-
-    public Lines getLines() {
-        return circularShifter.getLines();
-    }
-
-    public String getCirculatedLine(int[] wordIndex) {
-        return circularShifter.getCirculatedLine(wordIndex);
-    }
-
-
-    private void setSortedWordIndices() {
-
-        // Deep copy the word indices from circular shifter
-        ArrayList<int[]> wordIndices = circularShifter.getWordIndices();
-        sortedWordIndices = new ArrayList<>();
-        for (int[] index : wordIndices) {
-            sortedWordIndices.add(index.clone());
-        }
-
-        Collections.sort(sortedWordIndices, new IntArrayComparator(circularShifter));
-    }
-
-
-    /**
-     * This method defines a comparator that compares two indices based on the
-     * strings they refer to.
-     **/
-    private class IntArrayComparator implements Comparator<int[]>{
-        Shifter circularShifter;
-        public IntArrayComparator(Shifter circularShifter) {
-            this.circularShifter = circularShifter;
-        }
-        public int compare(int[] intArray1, int[] intArray2) {
-            String s1 = circularShifter.getCirculatedLine(intArray1);
-            String s2 = circularShifter.getCirculatedLine(intArray2);
-            StringComparator stringComparator = new StringComparator();
-            return stringComparator.compare(s1, s2);
-        }
-    }
-    /**
-     * This method defines a comparator that compares two strings based on their
-     * alphabetic content(sorting algorithm is a<A<b<B<...<z<Z).
-     **/
-    private class StringComparator implements Comparator<String> {
-        String alphabetString = " aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
-
-        public int compare(String s1, String s2) {
-            // If the two strings are identical, then return 0.
-            if (s1.equals(s2)) return 0;
-
-            // If the two strings are different, then compare their characters one after
-            // the other until the end of the shorter string is reached.
-            int minLength = Math.min(s1.length(), s2.length());
-
-            for (int i=0; i<minLength; i++) {
-                int s1Index = alphabetString.indexOf(s1.charAt(i));
-                int s2Index = alphabetString.indexOf(s2.charAt(i));
-                if (s1Index < s2Index) {
-                    return -1;
-                } else if (s1Index > s2Index) {
-                    return 1;
-                }
-            }
-            // The shorter string is smaller.
-            if (s1.length() < s2.length()) return -1;
-            else return 1;
-        }
-    }
+    void sort(Shifter shifter);
+    ArrayList<int[]> getSortedWordIndices();
+    String getCirculatedLine(int[] wordIndex);
 }
